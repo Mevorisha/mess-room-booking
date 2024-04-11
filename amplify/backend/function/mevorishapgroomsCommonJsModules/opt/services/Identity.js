88ca2130d12cb4c905a3b94bcb5058466df63357
AvirukBasak <avirukbasak@gmail.com>
2024-04-11 23:20:29 +0530

    Add functions for creating, updating, and deleting identities

--- amplify/backend/function/mevorishapgroomsCommonJsModules/opt/services/Identity.js
+++ amplify/backend/function/mevorishapgroomsCommonJsModules/opt/services/Identity.js
@@ -1,7 +1,58 @@
+const IdentityModel = require("../models/Identity");
+
 const IdentityError = {
   IDENTITY_NOT_FOUND: "IdenityModel:IDENTITY_NOT_FOUND",
 };
 
+/**
+ * @param {string?} aadhaarUrl
+ * @param {string?} idcardUrl
+ * @returns {Promise<IdentityModel.Identity>}
+ */
+async function createIdentity(aadhaarUrl, idcardUrl) {
+  return IdentityModel.create({
+    aadhaarImage: aadhaarUrl,
+    idcardImage: idcardUrl,
+  });
+}
+
+/**
+ * @param {IdentityModel.Identity | string} identity Identity model object or identity ID
+ * @param {string} aadhaarUrl
+ * @returns {Promise<void>}
+ */
+async function updateAadhaar(identity, aadhaarUrl) {
+  const identityId = typeof identity === "string" ? identity : identity._id;
+  await IdentityModel.updateOne(
+    { _id: identityId },
+    { aadhaarImage: aadhaarUrl }
+  );
+}
+
+/**
+ * @param {IdentityModel.Identity | string} identity Identity model object or identity ID
+ * @param {string} idcardUrl
+ * @returns {Promise<void>}
+ */
+async function updateIdcard(identity, idcardUrl) {
+  const identityId = typeof identity === "string" ? identity : identity._id;
+  await IdentityModel.updateOne(
+    { _id: identityId },
+    { idcardImage: idcardUrl }
+  );
+}
+
+/**
+ * @returns {Promise<void>}
+ */
+async function deleteExpired() {
+  await IdentityModel.deleteMany({ expiresAt: { $lt: new Date() } });
+}
+
 module.exports = {
   IdentityError,
+  createIdentity,
+  updateAadhaar,
+  updateIdcard,
+  deleteExpired,
 };
