720c16bea9920eef8a4d0da4493b7b06125b67fe
Aviruk Basak <avirukbasak@gmail.com>
2024-10-24 20:43:12 +0000

    update ErrorMessages.js and add getCleanFirebaseErrMsg function

--- app/src/modules/errors/ErrorMessages.js
+++ app/src/modules/errors/ErrorMessages.js
@@ -2,17 +2,35 @@
  * @enum {string}
  */
 const ErrorMessages = {
-    USER_NOT_LOGGED_IN: "User is not logged in.",
-    REGISTRATION_UNSUPPORTED: "Resgitration is not supported.",
-    REGISTRATION_FAILED: "Registration failed. Please try again.",
-    LOGIN_FAILED: "Login failed. Please try again.",
-    LOGOUT_FAILED: "Logout failed. Please try again.",
-    LOGGING_FAILED: "Logging to remote server failed.",
-    DATA_READ_FAILED: "Error reading data from the database.",
-    DATA_WRITE_FAILED: "Error writing data to the database.",
-    DATA_UPDATE_FAILED: "Error updating data in the database.",
-    DATA_DELETE_FAILED: "Error deleting data from the database.",
-    UNKNOWN_ERROR: "An unknown error occurred.",
+  USER_NOT_LOGGED_IN: "User is not logged in.",
+  REGISTRATION_UNSUPPORTED: "Resgitration is not supported.",
+  REGISTRATION_FAILED: "Registration failed. Please try again.",
+  LOGIN_FAILED: "Login failed. Please try again.",
+  LOGOUT_FAILED: "Logout failed. Please try again.",
+  LOGGING_FAILED: "Logging to remote server failed.",
+  DATA_READ_FAILED: "Error reading data from the database.",
+  DATA_WRITE_FAILED: "Error writing data to the database.",
+  DATA_UPDATE_FAILED: "Error updating data in the database.",
+  DATA_DELETE_FAILED: "Error deleting data from the database.",
+  UNKNOWN_ERROR: "An unknown error occurred.",
 };
 
 export default ErrorMessages;
+
+/**
+ * @param {any} error
+ */
+export function getCleanFirebaseErrMsg(error) {
+  if (error.code === "auth/user-not-found") {
+    return "User not found. Please register.";
+  }
+  if (error.code === "auth/invalid-credential") {
+    return "Invalid credentials. Please try again.";
+  }
+  if (error.code === "auth/email-already-in-use") {
+    return "Email already in use. Please login.";
+  }
+  return error
+    .toString()
+    .replace(/FirebaseError: Firebase: (.+) \(.+\/.+\)./g, "$1");
+}
