5722ba56e7ce06643e4d020ce2ddbad3715cbbae
Aviruk Basak <avirukbasak@gmail.com>
2024-10-24 12:26:06 +0000

    Refactor App component to include Notification component and routing logic

--- app/src/pages/App/index.jsx
+++ app/src/pages/App/index.jsx
@@ -1,24 +1,48 @@
-import React from "react";
-import logo from "../../assets/images/logo.svg";
-import "./styles.css";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, useNavigate } from "react-router-dom";
+
+import { isLoggedIn } from "../../modules/firebase/auth.js";
+import useNotification from "../../hooks/notification.js";
+
+import AuthPage from "../../pages/Auth";
+import Notification from "../../components/Notification";
+// import HomePage from "../../pages/Home";
+// import NotifPage from "../../pages/Notif";
+// import ProfilePage from "../../pages/Profile";
+// import AccountPage from "../../pages/Account";
 
 export default function App() {
+  const navigate = useNavigate();
+  const { notify, currentNotification } = useNotification();
+
+  // check if user is logged in
+  useEffect(() => {
+    isLoggedIn()
+      .then(() => navigate("/home"))
+      .catch(() => navigate("/auth"));
+  }, []);
+
+  // testing notifications
+  useEffect(() => {
+    notify("This is an info message", "info");
+    notify("This is a success message", "success");
+    notify("This is a warning message", "warning");
+    notify("This is an error message", "error");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Notification message={currentNotification.message} kind={currentNotification.kind} />
+
+      <BrowserRouter>
+        <Route path="/auth" Component={AuthPage} />
+        {/*
+          <Route path="/home" component={HomePage} />
+          <Route path="/notif" component={NotifPage} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/account" component={AccountPage} />
+        */}
+      </BrowserRouter>
+    </>
   );
 }
