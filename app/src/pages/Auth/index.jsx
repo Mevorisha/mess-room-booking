abf49ad7d6edb3e5818fded70a7631bc83f3fc5d
Aviruk Basak <avirukbasak@gmail.com>
2024-10-24 16:15:03 +0000

    Refactor Auth page: add login and register functionality, implement OAuth login options

--- app/src/pages/Auth/index.jsx
+++ app/src/pages/Auth/index.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { isLoggedIn } from "../../modules/firebase/auth.js";
-import "./styles.css";
+import {
+  isLoggedIn,
+  EmailPasswdAuth,
+  GoogleAuth,
+  AppleAuth,
+  MicrosoftAuth,
+} from "../../modules/firebase/auth.js";
 import useNotification from "../../hooks/notification.js";
+import ButtonText from "../../components/ButtonText";
+import "./styles.css";
 
 // write a auth page that has 2 tabs: login and register and also 2=3 buttons "Sign in with Google", "Sign in with Apple", "Sign in with Microsoft"
 export default function Auth() {
@@ -26,9 +33,123 @@ export default function Auth() {
       });
   }, [navigate, notify]);
 
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+
   return (
     <div className="Auth-page">
-      <div className="Auth-container"></div>
+      <div className="Auth-container">
+        <div className="button-container">
+          <ButtonText
+            title="Login"
+            onclick={() => setShowSection("login")}
+            rounded="left"
+            kind={showSection === "login" ? "primary" : "secondary"}
+            width="50%"
+          />
+          <ButtonText
+            title="Register"
+            onclick={() => setShowSection("register")}
+            rounded="right"
+            kind={showSection === "register" ? "primary" : "secondary"}
+            width="50%"
+          />
+        </div>
+        {showSection === "login" ? (
+          <form className="form-container">
+            <input
+              required
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              required
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="submit-container">
+              <ButtonText
+                title="Login"
+                rounded="all"
+                width="50%"
+                onclick={(e) => {
+                  e.preventDefault();
+                  EmailPasswdAuth.login(email, password);
+                }}
+              />
+            </div>
+          </form>
+        ) : (
+          <form className="form-container">
+            <input
+              required
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              required
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              required
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <div className="submit-container">
+              <ButtonText
+                title="Register"
+                rounded="all"
+                width="50%"
+                onclick={(e) => {
+                  e.preventDefault();
+                  if (password === confirmPassword) {
+                    EmailPasswdAuth.register(email, password);
+                  } else {
+                    notify("Passwords do not match", "error");
+                    console.error("Passwords do not match");
+                  }
+                }}
+              />
+            </div>
+          </form>
+        )}
+        {/* implement login with Google, Apple & Microsoft */}
+        <div className="oauth-container">
+          <div className="oauth-button">
+            <img
+              style={{ paddingLeft: "1px" }}
+              src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
+              alt="google"
+            />
+          </div>
+          <div className="oauth-button">
+            <img
+              style={{ height: "27px", paddingBottom: "1.5px" }}
+              src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"
+              alt="apple"
+            />
+          </div>
+          <div className="oauth-button">
+            <img
+              style={{ height: "24px" }}
+              src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg"
+              alt="microsoft"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
