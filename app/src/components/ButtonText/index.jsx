0d92a9650307cba16626e6dafbc24e2ff203886c
Aviruk Basak <avirukbasak@gmail.com>
2024-10-24 16:14:25 +0000

    Refactor ButtonText component to support rounded corners and dynamic width

--- app/src/components/ButtonText/index.jsx
+++ app/src/components/ButtonText/index.jsx
@@ -5,20 +5,27 @@ import "./styles.css";
  * @param {{
  *   title: string,
  *   onclick: () => void,
- *   rounded?: boolean,
+ *   rounded?: "all" | "left" | "right" | "top" | "bottom" | "none",
  *   kind?: "primary" | "secondary",
+ *   width?: "default" | "full" | string
  * }} props
  */
 export default function ButtonText({
   title,
   onclick,
-  rounded = false,
+  rounded = "none",
   kind = "primary",
+  width = "default",
 }) {
   let classes = [];
-  if (rounded) {
-    classes.push("ButtonText-rounded");
-  }
+  let borderRadiusStyle = {
+    all: "var(--rounded-rad)",
+    left: "var(--rounded-rad) 0 0 var(--rounded-rad)",
+    right: "0 var(--rounded-rad) var(--rounded-rad) 0",
+    top: "var(--rounded-rad) var(--rounded-rad) 0 0",
+    bottom: "0 0 var(--rounded-rad) var(--rounded-rad)",
+    none: "0",
+  };
 
   if (kind === "primary") {
     classes.push("ButtonText-primary");
@@ -26,8 +33,16 @@ export default function ButtonText({
     classes.push("ButtonText-secondary");
   }
 
+  if (width === "full") {
+    width = "100%";
+  }
+
   return (
-    <button className={classes.join(" ")} onClick={onclick}>
+    <button
+      className={classes.join(" ")}
+      onClick={onclick}
+      style={{ width, borderRadius: borderRadiusStyle[rounded] }}
+    >
       {title}
     </button>
   );
