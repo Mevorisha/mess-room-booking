483674100be04aceb26bf493801d1acdef64257d
Aviruk Basak <avirukbasak@gmail.com>
2024-10-24 14:15:18 +0000

    Refactor Auth page to handle logged out users and display a notification

--- app/src/pages/Auth/index.jsx
+++ app/src/pages/Auth/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { isLoggedIn } from "../../modules/firebase/auth.js";
 import "./styles.css";
+import useNotification from "../../hooks/notification.js";
 
 // write a auth page that has 2 tabs: login and register and also 2=3 buttons "Sign in with Google", "Sign in with Apple", "Sign in with Microsoft"
 export default function Auth() {
@@ -13,13 +14,17 @@ export default function Auth() {
   );
 
   const navigate = useNavigate();
+  const notify = useNotification();
 
   // check if user is logged in
   useEffect(() => {
     isLoggedIn()
       .then(() => navigate("/home"))
-      .catch(() => navigate("/auth"));
-  }, []);
+      .catch(() => {
+        navigate("/auth");
+        notify("You are not logged in", "warning");
+      });
+  }, [navigate, notify]);
 
   return (
     <div className="Auth-page">
