882796551dda58b87b2f146ade67ba7a4543e36f
AvirukBasak <avirukbasak@gmail.com>
2024-10-26 21:28:48 +0530

    update useAuth hook to unsubscribe from onAuthStateChanged during useEffect cleanup

--- app/src/hooks/auth.js
+++ app/src/hooks/auth.js
@@ -3,18 +3,27 @@ import { onAuthStateChanged } from "../modules/firebase/auth.js";
 import useNotification from "./notification.js";
 import AuthContext from "../contexts/auth.js";
 
+/**
+ * @returns {{ uid: string } | null}
+ */
 export default function useAuth() {
   const { user, setUser } = useContext(AuthContext);
   const notify = useNotification();
 
   useEffect(() => {
-    onAuthStateChanged((useruid) => {
+    let unsubscribeOutside = () => {};
+
+    onAuthStateChanged((useruid, unsubscribeInside) => {
+      unsubscribeOutside = unsubscribeInside;
+
       if (useruid) setUser({ uid: useruid });
       else setUser({ uid: "" });
 
       if (!useruid) notify("You are not logged in", "warning");
     });
+
+    return () => unsubscribeOutside();
   }, [setUser]);
 
-  return user;
+  return user.uid ? user : null;
 }
