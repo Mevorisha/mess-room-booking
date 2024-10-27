7f1aeebaaf74ba60c1a5340653d6ff0f056d6464
AvirukBasak <avirukbasak@gmail.com>
2024-10-28 04:24:01 +0530

    moved useEffects from hook to context and added code to load additional user data from db if authState becomes "LOGGED_IN" which in turn updates the user in AuthContext

--- app/src/contexts/auth.js
+++ app/src/contexts/auth.js
@@ -1,36 +1,121 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import { RtDbPaths } from "../modules/firebase/init.js";
+import { onAuthStateChanged } from "../modules/firebase/auth.js";
+import { onDbContentChange } from "../modules/firebase/db.js";
+import useNotification from "../hooks/notification.js";
+
+/**
+ * @enum {string}
+ */
+export const AuthState = {
+  STILL_LOADING: "STILL_LOADING",
+  NOT_LOGGED_IN: "NOT_LOGGED_IN",
+  LOGGED_IN: "LOGGED_IN",
+};
 
 export class User {
   /**
    * @param {string} uid
-   * @param {"TENANT" | "OWNER" | ""} type
+   * @param {"TENANT" | "OWNER" | "EMPTY"} type
    * @param {string} photoURL
    */
-  constructor(uid, type, photoURL) {
+  constructor(uid, type = "EMPTY", photoURL = "") {
     this.uid = uid;
     this.type = type;
     this.photoURL = photoURL;
   }
 
+  /**
+   * @returns {User}
+   */
   static empty() {
-    return new User("", "", "");
+    return new User("");
   }
 }
 
 const AuthContext = createContext({
-  /** @type {User | null} */
-  user: null,
-  /** @type {(user: User | null) => void} */
+  /** @type {AuthState} */
+  state: AuthState.STILL_LOADING,
+  /** @type {User} */
+  user: User.empty(),
+  /** @type {(user: User) => void} */
   setUser: () => {},
 });
 
 export default AuthContext;
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(/** @type {User | null} */ (null));
+  const [authState, setAuthState] = useState(AuthState.STILL_LOADING);
+  const [userUid, setUserUid] = useState("");
+  const [finalUser, setFinalUser] = useState(User.empty());
+
+  const notify = useNotification();
+
+  /* A bit of knowledge for my future confused self:
+   * useEffect is a hook that runs with following conditions:
+   *     - the first time the component is rendered
+   *     - when one of the dependencies changes
+   * By this logic, useEffect should run only once, right? But in dev mode,
+   * useEffect runs twice. This is documented in the following link:
+   *     https://react.dev/learn/synchronizing-with-effects#how-to-handle-the-effect-firing-twice-in-development
+   *     Docs guarantee that useEffect will run once in production mode so :shrug:
+   * On the first run, an auth listener is started.
+   * On the 2nd run, a new auth listener is started. So the unsubscribe function
+   * of the first listener is returned from useEffect as a cleanup function.
+   * This stops the first listener and keeps only the 2nd listener active.
+   */
+
+  /* listen for auth state changes and update the temporary user */
+  useEffect(() => {
+    console.error("onAuthStateChanged started");
+
+    const unsubscribe = onAuthStateChanged((uid) => {
+      if (null == uid) setAuthState(AuthState.NOT_LOGGED_IN);
+      else {
+        setAuthState(AuthState.LOGGED_IN);
+        setUserUid(uid);
+        setFinalUser(new User(uid));
+      }
+
+      console.error(
+        `onAuthStateChanged updated, old = ${userUid}, new = ${uid}`
+      );
+      if (null == uid) notify("You are not logged in", "warning");
+    });
+
+    return () => {
+      console.error("onAuthStateChanged stopped");
+      unsubscribe();
+    };
+  }, [setUserUid, notify]);
+
+  /* listen for changes at rtdb at RtDbPaths.IDENTITY/user.uid/ if temp user is updated
+   * and update the final user with additional data from rtdb */
+  useEffect(() => {
+    if (authState !== AuthState.LOGGED_IN) return;
+
+    console.error("onDbContentChange started");
+
+    const unsubscribe = onDbContentChange(
+      RtDbPaths.IDENTITY,
+      `${userUid}/`,
+      (data) => {
+        console.error("onDbContentChange updated, data = ", data);
+        if (!data) setFinalUser(new User(userUid));
+        else setFinalUser(new User(userUid, data.type, data.photoURL ?? ""));
+      }
+    );
+
+    return () => {
+      console.error("onDbContentChange stopped, finaluser");
+      unsubscribe();
+    };
+  }, [authState, userUid, setFinalUser]);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider
+      value={{ state: authState, user: finalUser, setUser: setFinalUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- app/src/hooks/auth.js
+++ app/src/hooks/auth.js
@@ -1,48 +1,10 @@
-import { useEffect, useContext } from "react";
-import { onAuthStateChanged } from "../modules/firebase/auth.js";
-import useNotification from "./notification.js";
-import AuthContext, { User } from "../contexts/auth.js";
-import { getUserDetailsFromUID } from "../modules/firebase/auth.js";
+import { useContext } from "react";
+import AuthContext, { AuthState, User } from "../contexts/auth.js";
 
 /**
- * @returns {User | null} If null, state is loading. If empty string, user is not logged in. If not empty, user is logged in.
+ * @returns {{ state: AuthState, user: User }}
  */
 export default function useAuth() {
-  const { user, setUser } = useContext(AuthContext);
-  const notify = useNotification();
-
-  /* A bit of knowledge for my future confused self:
-   * useEffect is a hook that runs with following conditions:
-   *     - the first time the component is rendered
-   *     - when one of the dependencies changes
-   * By this logic, useEffect should run only once, right? But in dev mode,
-   * useEffect runs twice. This is documented in the following link:
-   *     https://react.dev/learn/synchronizing-with-effects#how-to-handle-the-effect-firing-twice-in-development
-   *     Docs guarantee that useEffect will run once in production mode so :shrug:
-   * On the first run, an auth listener is started.
-   * On the 2nd run, a new auth listener is started. So the unsubscribe function
-   * of the first listener is returned from useEffect as a cleanup function.
-   * This stops the first listener and keeps only the 2nd listener active.
-   */
-
-  useEffect(() => {
-    // console.error("onAuthStateChanged started, uid = ", user.uid);
-    const unsubscribe = onAuthStateChanged((useruid) => {
-      if (useruid) {
-        getUserDetailsFromUID(useruid)
-          .then((user) => setUser(user))
-          .catch((e) => notify(e.toString(), "error"));
-      } else {
-        setUser(User.empty());
-      }
-      // console.error("onAuthStateChanged updated, uid = ", useruid);
-      if (!useruid) notify("You are not logged in", "warning");
-    });
-    return () => {
-      // console.error("onAuthStateChanged stopped, uid = ", user.uid);
-      unsubscribe();
-    };
-  }, [setUser, notify]);
-
-  return user;
+  const { state, user } = useContext(AuthContext);
+  return { state, user };
 }
