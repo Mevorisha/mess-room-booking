d4c0f4727cb425a759125a0e2585384a8d582679
AvirukBasak <avirukbasak@gmail.com>
2024-10-29 00:51:18 +0530

    removed onboarding code

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -1,58 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthState, User } from "../../contexts/auth";
+import { User } from "../../contexts/auth";
 import useAuth from "../../hooks/auth.js";
 import ButtonText from "../../components/ButtonText";
 
 // @ts-ignore
 import dpMevorisha from "../../assets/images/dpMevorisha.png";
 import "./styles.css";
-
-/**
- * @param {{ auth: {
- *   state: AuthState;
- *   user: User;
- *   updateUserDetailsInDb: (type: "TENANT" | "OWNER" | "EMPTY", photoURL: string) => void;
- * } }} props
- */
-function SelectInitialType({ auth }) {
-  const [accountType, setAccountType] = useState(
-    /** @type {"TENANT" | "OWNER" | "EMPTY"} */ ("EMPTY")
-  );
-
-  useEffect(() => {
-    if ("EMPTY" === accountType) return;
-    // write the account type to the database
-    auth.updateUserDetailsInDb(accountType, auth.user.photoURL);
-  }, [auth.user.type, accountType]);
-
-  return (
-    <div className="pages-Home">
-      <div className="select-type-container">
-        <h1>Choose Profile Type</h1>
-        <h4>Profile type can be changed later</h4>
-
-        <div className="desc">
-          <p>If you are going to stay in a room, select Tenant.</p>
-          <p> If you are the owner giving a room for rent select Owner.</p>
-        </div>
-
-        <ButtonText
-          rounded="all"
-          title="Tenant"
-          kind="primary"
-          onclick={() => setAccountType("TENANT")}
-        />
-        <ButtonText
-          rounded="all"
-          title="Owner"
-          kind="primary"
-          onclick={() => setAccountType("OWNER")}
-        />
-      </div>
-    </div>
-  );
-}
 
 /**
  * @param {{ children }} props
@@ -64,11 +18,11 @@ function TopBar({ children }) {
         <img src={dpMevorisha} alt="logo" />
         <h1>Mevorisha</h1>
       </div>
-      <div className="section-buttons-container">
-        {children}
-      </div>
+      <div className="section-buttons-container">{children}</div>
       <div className="action-buttons-container">
-        <span><i style={{ fontSize: "1rem" }} className="fa fa-bars"></i></span>
+        <span>
+          <i style={{ fontSize: "1rem" }} className="fa fa-bars"></i>
+        </span>
       </div>
     </div>
   );
@@ -82,7 +36,12 @@ function HomeForTenant({ user }) {
     <div className="pages-Home">
       <TopBar>
         <ButtonText rounded="all" title="Rooms" kind="primary" width="50%" />
-        <ButtonText rounded="all" title="Booking" kind="cannibalized" width="50%" />
+        <ButtonText
+          rounded="all"
+          title="Booking"
+          kind="cannibalized"
+          width="50%"
+        />
       </TopBar>
       <div className="content-container">
         <div className="contents">
@@ -105,7 +64,12 @@ function HomeForOwner({ user }) {
     <div className="pages-Home">
       <TopBar>
         <ButtonText rounded="all" title="Rooms" kind="primary" width="50%" />
-        <ButtonText rounded="all" title="Booking" kind="cannibalized" width="50%" />
+        <ButtonText
+          rounded="all"
+          title="Booking"
+          kind="cannibalized"
+          width="50%"
+        />
       </TopBar>
       <div className="content-container">
         <div className="contents">
@@ -123,13 +87,6 @@ function HomeForOwner({ user }) {
 export default function Home() {
   const auth = useAuth();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (auth.state === AuthState.STILL_LOADING) navigate("/");
-  }, [auth.state, navigate]);
-
-  if (auth.state === AuthState.STILL_LOADING) return null;
-  if (auth.user.type === "EMPTY") return <SelectInitialType auth={auth} />;
 
   return auth.user.type === "TENANT" ? (
     <HomeForTenant user={auth.user} />
