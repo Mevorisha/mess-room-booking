07a838f44d7ca83a47f2f80bfae4997172681f78
AvirukBasak <avirukbasak@gmail.com>
2024-10-29 22:22:57 +0530

    log errors to console as well

--- app/src/modules/firebase/util.js
+++ app/src/modules/firebase/util.js
@@ -1,6 +1,8 @@
 import { fbRtdbGetRef, RtDbPaths } from "./init.js";
 import { set } from "firebase/database";
-import ErrorMessages, { getCleanFirebaseErrMsg } from "../errors/ErrorMessages.js"
+import ErrorMessages, {
+  getCleanFirebaseErrMsg,
+} from "../errors/ErrorMessages.js";
 
 /**
  * @param {string} operation
@@ -26,12 +28,12 @@ async function logInfo(operation, descrip, code = "code_unknown") {
  */
 async function logError(operation, descrip, code = "code_unknown") {
   const uid = localStorage.getItem("uid") || "user_logged_out";
+  console.error(`I: ${uid}: ${operation}: ${code}: ${descrip}`);
   try {
     const timestamp = new Date().toUTCString();
     const logRef = fbRtdbGetRef(RtDbPaths.LOGS, timestamp);
     await set(logRef, `E: ${uid}: ${operation}: ${code}: ${descrip}`);
   } catch (error) {
-    console.error(`I: ${uid}: ${operation}: ${code}: ${descrip}`);
     console.error(ErrorMessages.LOGGING_FAILED, getCleanFirebaseErrMsg(error));
   }
 }
