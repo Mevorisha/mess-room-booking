703f5b8fd8e127b710313bd60ef5a7326d32f7ff
AvirukBasak <avirukbasak@gmail.com>
2024-10-31 10:36:10 +0530

    split updateProfileMobileNo into send and verify otp functions

--- app/src/contexts/auth.js
+++ app/src/contexts/auth.js
@@ -353,34 +353,43 @@ export function AuthProvider({ children }) {
     [updateUserDetailsInDb, notify]
   );
 
-  const updateProfileMobileNo = useCallback(
-    /**
-     * @param {string} mobile
-     * @param {() => Promise<string>} getOtpFromUser - You need to define this function in your component to get OTP from user
-     */
-    (mobile, getOtpFromUser) => {
-      LinkMobileNumber.sendOtp(mobile)
-        .then(() => getOtpFromUser())
-        .then((otp) =>
-          LinkMobileNumber.verifyOtp(otp)
-            .then((result) => {
-              if (!result)
-                return Promise.reject("Mobile number verification failed");
-              return Promise.resolve();
-            })
-            .catch(async (error) => {
-              if (error?.code !== "auth/provider-already-linked")
-                return Promise.reject(error);
-              await LinkMobileNumber.unlinkPhoneNumber();
-              return await LinkMobileNumber.verifyOtp(otp);
-            })
-        )
-        .then(() => updateUserDetailsInDb({ mobile }))
-        .then(() => notify("Mobile number updated successfully", "success"))
-        .catch((e) => notify(e.toString(), "error"));
+  const sendPhoneVerificationCode = useCallback(
+    /** @param {string} number */ async (number) => {
+      return LinkMobileNumber.sendOtp(number).then(() =>
+        notify("Check your mobile for OTP", "info")
+      );
     },
-    [updateUserDetailsInDb, notify]
+    [notify]
   );
+
+  const verifyPhoneVerificationCode = useCallback(
+    /**
+     * @param {string} otp
+     */
+    async (otp) => {
+      LinkMobileNumber.verifyOtp(otp)
+        .then((result) => {
+          if (!result)
+            return Promise.reject("Mobile number verification failed");
+          return Promise.resolve();
+        })
+        .catch(async (error) => {
+          if (error?.code !== "auth/provider-already-linked")
+            return Promise.reject(error);
+          await LinkMobileNumber.unlinkPhoneNumber();
+          return await LinkMobileNumber.verifyOtp(otp);
+        })
+        // .then(() => updateUserDetailsInDb({ mobile: finalUser.mobile }))
+        .then(() => notify("Mobile number verified successfully", "success"));
+    },
+    [notify /* updateUserDetailsInDb, finalUser.mobile */]
+  );
+
+  const unlinkPhoneNumber = useCallback(async () => {
+    LinkMobileNumber.unlinkPhoneNumber()
+      // .then(() => removeUserDetailsInDb([UserDetailsEnum.mobile]))
+      .then(() => notify("Mobile number unlinked successfully", "success"));
+  }, [notify /*, removeUserDetailsInDb */]);
 
   return (
     <AuthContext.Provider
@@ -392,7 +401,9 @@ export function AuthProvider({ children }) {
         updateProfileType,
         updateProfilePhoto,
         updateProfileName,
-        updateProfileMobileNo,
+        sendPhoneVerificationCode,
+        verifyPhoneVerificationCode,
+        unlinkPhoneNumber,
       }}
     >
       {children}
