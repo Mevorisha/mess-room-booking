1843cb08e84318c303f5409c00b1ec3e37884e64
AvirukBasak <avirukbasak@gmail.com>
2024-11-20 06:47:45 +0530

    added fn for converting byte to human readable unit

---
+++ src/modules/util/unitConversion.js
@@ -0,0 +1,18 @@
+/**
+ * Converts bytes to human readable format
+ * @param {number} bytes
+ * @returns {string}
+ */
+export function sizehuman(bytes) {
+  const thresh = 1024;
+  if (Math.abs(bytes) < thresh) {
+    return bytes + " B";
+  }
+  const units = ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  let u = -1;
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+  return bytes.toFixed(1) + " " + units[u];
+}
