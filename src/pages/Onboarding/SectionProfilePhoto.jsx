5ebef98c2b4df4ae542f811226da364a515681a9
AvirukBasak <avirukbasak@gmail.com>
2024-11-20 09:37:42 +0530

    show dialog box with large image preview when clicked on profile image

--- src/pages/Onboarding/SectionProfilePhoto.jsx
+++ src/pages/Onboarding/SectionProfilePhoto.jsx
@@ -4,14 +4,38 @@ import { loadFileFromFilePicker } from "../../modules/firebase/storage.js";
 import { PageUrls } from "../../modules/util/pageUrls.js";
 import useAuth from "../../hooks/auth.js";
 import useNotification from "../../hooks/notification.js";
+import useDialog from "../../hooks/dialogbox.js";
 import ButtonText from "../../components/ButtonText";
 
 // @ts-ignore
 import dpGeneric from "../../assets/images/dpGeneric.png";
 
+/**
+ * @param {{ largeImageUrl: string }} props
+ * @returns {React.JSX.Element}
+ */
+function DialogContent({ largeImageUrl }) {
+  const dialog = useDialog();
+
+  return (
+    <div
+      className="pages-Onboarding-ProfilePhoto-DialogContent"
+    >
+      <img src={largeImageUrl} alt="profile" />
+      <i
+        className="btn-close fa fa-close"
+        onClick={() => {
+          dialog.hide();
+        }}
+      />
+    </div>
+  );
+}
+
 export default function SetProfilePhoto() {
   const auth = useAuth();
   const notify = useNotification();
+  const dialog = useDialog();
   const navigate = useNavigate();
 
   // state
@@ -45,6 +69,14 @@ export default function SetProfilePhoto() {
       });
   }
 
+  function handleShowLargeImage() {
+    if (!auth.user.photoURLs?.large) return;
+
+    dialog.show(
+      <DialogContent largeImageUrl={auth.user.photoURLs?.large} />
+    );
+  }
+
   return (
     <div className="pages-Onboarding">
       <div className="onboarding-container">
@@ -59,7 +91,7 @@ export default function SetProfilePhoto() {
         </div>
 
         <div className="photo-container">
-          <img src={photoURL} alt="profile" />
+          <img src={photoURL} alt="profile" onClick={handleShowLargeImage} />
           <ButtonText
             rounded="all"
             title="Update Photo"
