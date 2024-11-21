7b8ef74a76478db369b4210c704cabb2e20f5cf2
AvirukBasak <avirukbasak@gmail.com>
2024-11-21 19:26:07 +0530

    refactored resizeImage to auto handle size for missing dimension

--- src/modules/util/dataConversion.js
+++ src/modules/util/dataConversion.js
@@ -20,19 +20,17 @@ export function sizehuman(bytes) {
 /**
  * Resizes an image blob
  * @param {File} file
- * @param {number} maxWidth
- * @param {number} maxHeight
+ * @param {{ w?: number, h?: number }} target
  * @param {string} mimeType
- * @param {number} quality
+ * @param {number | undefined} quality
  * @returns {Promise<File>}
  * @throws {Error}
  */
-export function resizeImageBlob(
+export function resizeImage(
   file,
-  maxWidth,
-  maxHeight,
+  { w: targetWidth, h: targetHeight },
   mimeType = "image/jpeg",
-  quality = 0.8
+  quality = undefined
 ) {
   return new Promise((resolve, reject) => {
     const filename = file.name;
@@ -40,33 +38,52 @@ export function resizeImageBlob(
     const url = URL.createObjectURL(file);
 
     img.onload = () => {
-      URL.revokeObjectURL(url); // Clean up URL object
+      // Clean up URL object
+      URL.revokeObjectURL(url);
 
-      // Calculate new dimensions
-      let { width, height } = img;
-      if (width > maxWidth || height > maxHeight) {
-        const aspectRatio = width / height;
-        if (width > height) {
-          width = maxWidth;
-          height = Math.floor(maxWidth / aspectRatio);
-        } else {
-          height = maxHeight;
-          width = Math.floor(maxHeight * aspectRatio);
-        }
+      // Get image aspect ratio
+      const aspectRatio = img.width / img.height;
+      let resultWidth = 0,
+        resultHeight = 0;
+
+      // neither width nor height is specified, keep original size
+      if (!targetWidth && !targetHeight) {
+        resultWidth = img.width;
+        resultHeight = img.height;
+      }
+
+      // if width is not specified, but height is, calculate width based on aspect ratio
+      else if (!targetWidth && targetHeight) {
+        resultWidth = Math.floor(targetHeight * aspectRatio);
+      }
+
+      // if height is not specified, but width is, calculate height based on aspect ratio
+      else if (targetWidth && !targetHeight) {
+        resultHeight = Math.floor(targetWidth / aspectRatio);
+      }
+
+      // if both width and height are specified, use it
+      else if (targetWidth && targetHeight) {
+        resultWidth = targetWidth;
+        resultHeight = targetHeight;
       }
 
       // Create a canvas and resize the image
       const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
+      canvas.width = resultWidth;
+      canvas.height = resultHeight;
+
       const ctx = canvas.getContext("2d");
+
       if (!ctx) {
         reject(new Error("Canvas creation failed"));
         return;
       }
-      ctx.drawImage(img, 0, 0, width, height);
 
-      // Convert the canvas back to a blob
+      // Draw the image with the specified width and height
+      ctx.drawImage(img, 0, 0, resultWidth, resultHeight);
+
+      // Convert the canvas back to a File
       canvas.toBlob(
         (resizedBlob) => {
           if (resizedBlob)
