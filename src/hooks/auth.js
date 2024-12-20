f1dd5210a0147e4e0a5d145768b819ef59fa836e
AvirukBasak <avirukbasak@gmail.com>
2024-12-21 02:26:34 +0530

    change useAuth hook to useUsrCopositeCtx that combines all fn of old auth context into a single object

--- src/hooks/auth.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "../contexts/auth.js";
-
-/**
- * @returns {import("../contexts/auth.js").AuthContextType}
- */
-export default function useAuth() {
-  return useContext(AuthContext);
-}
---
+++ src/hooks/compositeUser.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import AuthContext from "../contexts/auth.js";
+import AccountContext from "../contexts/account.js";
+import ProfileContext from "../contexts/profile.js";
+import IdentityContext from "../contexts/identity.js";
+import UserContext from "../contexts/user.js";
+
+/**
+ * @returns {{
+ *   userCtx: import("../contexts/user.js").UserContextType;
+ *   authCtx: import("../contexts/auth.js").AuthContextType;
+ *   accountCtx: import("../contexts/account.js").AccountContextType;
+ *   profileCtx: import("../contexts/profile.js").ProfileContextType;
+ *   identityCtx: import("../contexts/identity.js").IdentityContextType;
+ * }}
+ */
+export default function useUsrCompositeCtx() {
+  return {
+    userCtx: useContext(UserContext),
+    authCtx: useContext(AuthContext),
+    accountCtx: useContext(AccountContext),
+    profileCtx: useContext(ProfileContext),
+    identityCtx: useContext(IdentityContext),
+  };
+}
