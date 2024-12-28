6235c85d57ca9c26c124a0ac6c4e0c94a66fbd77
AvirukBasak <avirukbasak@gmail.com>
2024-12-28 15:35:00 +0530

    refactor: implement Profile component using useCompositeUser hook and add styles

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-import PageNotFound from "../PageNotFound";
+import useCompositeUser from "../../hooks/compositeUser";
+
+import "./styles.css";
 
 /**
  * @returns {React.JSX.Element}
  */
 export default function Profile() {
-  return <PageNotFound />;
+  const compUsr = useCompositeUser();
+
+  return (
+    <div className="pages-Profile">
+      <div className="container">
+        <h1>
+          {compUsr.userCtx.user.type === "OWNER" ? "Owner" : "Tenant"} Profile
+        </h1>
+        <h4>Profile details will be visible publicly.</h4>
+        <div className="desc">
+          <p>
+            Documents like work or institution identity card and aadhaar card
+            may be used by you room owner to verify your identity.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
