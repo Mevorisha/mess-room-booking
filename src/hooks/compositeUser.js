84dc83924995af475dfc2f1c2f646e36940df4ed
AvirukBasak <avirukbasak@gmail.com>
2024-12-28 15:32:55 +0530

    refactor: rename useUsrCompositeCtx to useCompositeUser and update JSDoc for clarity

--- src/hooks/compositeUser.js
+++ src/hooks/compositeUser.js
@@ -6,15 +6,17 @@ import IdentityContext from "../contexts/identity.js";
 import UserContext from "../contexts/user.js";
 
 /**
- * @returns {{
+ * @typedef {{
  *   userCtx: import("../contexts/user.js").UserContextType;
  *   authCtx: import("../contexts/auth.js").AuthContextType;
  *   accountCtx: import("../contexts/account.js").AccountContextType;
  *   profileCtx: import("../contexts/profile.js").ProfileContextType;
  *   identityCtx: import("../contexts/identity.js").IdentityContextType;
- * }}
+ * }} CompositeUserContextType
+ *
+ * @returns {CompositeUserContextType}
  */
-export default function useUsrCompositeCtx() {
+export default function useCompositeUser() {
   return {
     userCtx: useContext(UserContext),
     authCtx: useContext(AuthContext),
