8d463d97d67e348dae7e153d5de5396e8fe249e8
AvirukBasak <avirukbasak@gmail.com>
2025-02-18 14:16:23 +0530

    reload page on setLang if old and new lang are different

--- src/contexts/language.js
+++ src/contexts/language.js
@@ -1,7 +1,9 @@
 import React, { useState, createContext, useCallback } from "react";
 
 const LangContext = createContext({
-  lang: /** @type {"ENGLISH" | "BANGLA" | "HINDI"} */ ("ENGLISH"),
+  lang: /** @type {"ENGLISH" | "BANGLA" | "HINDI"} */ (
+    window.localStorage.getItem("lang") || "ENGLISH"
+  ),
   setLang: /** @type {(val: "ENGLISH" | "BANGLA" | "HINDI") => void} */ (
     () => {}
   ),
@@ -19,8 +21,7 @@ export function LanguageProvider({ children }) {
   const [lang, _setLang] = useState(
     /** @type {() => "ENGLISH" | "BANGLA" | "HINDI"} */ (
       () => {
-        const storedLang = window.localStorage.getItem("lang");
-        const newLangSt = storedLang || "ENGLISH";
+        const newLangSt = window.localStorage.getItem("lang") || "ENGLISH";
         window.localStorage.setItem("lang", newLangSt);
         return newLangSt;
       }
@@ -31,12 +32,13 @@ export function LanguageProvider({ children }) {
     /**
      * @param {"ENGLISH" | "BANGLA" | "HINDI"} newVal
      */
-    (newVal) => {
-      _setLang(newVal);
-      window.localStorage.setItem("lang", newVal);
-      // ensure all modules are reloaded with the new language value
-      window.location.reload();
-    },
+    (newVal) =>
+      _setLang((oldVal) => {
+        window.localStorage.setItem("lang", newVal);
+        // ensure all modules are reloaded with the new language value
+        if (oldVal !== newVal) window.location.reload();
+        return newVal;
+      }),
     [_setLang]
   );
 
