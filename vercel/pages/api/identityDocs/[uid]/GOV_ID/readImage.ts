3a8948a7f4e998832cd8038e53ff4120b64e643b
AvirukBasak <avirukbasak@gmail.com>
2025-02-28 03:08:07 +0530

    feat: implement readImage API for GOV_ID and enhance size parameter handling

--- vercel/pages/api/identityDocs/[uid]/GOV_ID/readImage.ts
+++ vercel/pages/api/identityDocs/[uid]/GOV_ID/readImage.ts
@@ -1,10 +1,11 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import Identity, { SchemaFields } from "../../../../models/Identity.js";
-import { respond } from "../../../../lib/utils/respond.js";
+import Identity, { SchemaFields } from "../../../../../models/Identity.js";
+import { respond } from "../../../../../lib/utils/respond.js";
+import { authenticate } from "../../../../../middlewares/auth.js";
 
 /**
  * ```
- * request = "GET /api/profile/[uid]/readImage?size=(small|medium|large)"
+ * request = "GET /api/identityDocs/[uid]/GOV_ID/readImage?size=(small|medium|large)"
  * response = 301 to "Content-Type: image/(jpeg|png)"
  * ```
  */
@@ -26,12 +27,16 @@ export default async function GET(req: VercelRequest, res: VercelResponse) {
     return respond(res, { status: 400, error: "Invalid query 'size: small | medium | large'" });
   }
   try {
-    const profile = await Identity.get(uid, [SchemaFields.PROFILE_PHOTOS]);
-    if (!profile?.profilePhotos?.large) {
+    const profile = await Identity.get(uid, [SchemaFields.IDENTITY_PHOTOS]);
+    if (!profile?.identityPhotos?.govid) {
       return respond(res, { status: 404, error: "Image not found" });
     }
+    if (profile.identityPhotos.govid.isPrivate) {
+      // Require authentication middleware
+      if (!(await authenticate(req, res))) return;
+    }
     // Redirect to the image URL
-    return res.redirect(301, profile?.profilePhotos[size]);
+    return res.redirect(301, profile?.identityPhotos?.govid[size]);
   } catch (e) {
     return respond(res, { status: e.status ?? 500, error: e.message });
   }
