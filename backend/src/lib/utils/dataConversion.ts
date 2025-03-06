8b68c74ca6ba22996a2e77918cd2fbdf77d26bc3
AvirukBasak <avirukbasak@gmail.com>
2025-03-06 12:50:32 +0530

    fix: optimize image resizing by removing unnecessary width and height parameters

--- backend/src/lib/utils/dataConversion.ts
+++ backend/src/lib/utils/dataConversion.ts
@@ -6,9 +6,9 @@ export async function resizeImage(
   imageBuffer: Buffer
 ): Promise<{ small: ImgWithSz<30>; medium: ImgWithSz<90>; large: ImgWithSz<500> }> {
   const promises = [
-    sharp(imageBuffer).resize(30, 30).toBuffer(),
-    sharp(imageBuffer).resize(90, 90).toBuffer(),
-    sharp(imageBuffer).resize(500, 500).toBuffer(),
+    sharp(imageBuffer).resize(30).toBuffer(),
+    sharp(imageBuffer).resize(90).toBuffer(),
+    sharp(imageBuffer).resize(500).toBuffer(),
   ];
   const [small, medium, large] = await Promise.all(promises);
   return {
