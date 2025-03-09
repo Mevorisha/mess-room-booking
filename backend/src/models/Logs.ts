a86c4208af3ad26ae234d6eee6bf796af76f4bcd
AvirukBasak <avirukbasak@gmail.com>
2025-03-09 22:41:35 +0530

    refactor: streamline Logs model by removing try-catch blocks and relying on promise rejection

--- backend/src/models/Logs.ts
+++ backend/src/models/Logs.ts
@@ -1,5 +1,4 @@
 import { FirestorePaths } from "@/lib/firebaseAdmin/init";
-import { CustomApiError } from "@/lib/utils/ApiError";
 
 export type LogType = "info" | "error" | "warn";
 export type DateTimeRange = { from: Date; to: Date };
@@ -18,11 +17,8 @@ class Logs {
   static async put(uid: string, log: { timestamp: string; message: string; type: LogType }): Promise<void> {
     const docRef = FirestorePaths.Logs(uid);
     const { type, timestamp, message } = log;
-    try {
-      await docRef.set({ [timestamp]: { message, type } }, { merge: true });
-    } catch (e) {
-      return Promise.reject(CustomApiError.create(500, e.message));
-    }
+
+    await docRef.set({ [timestamp]: { message, type } }, { merge: true });
   }
 
   /**
@@ -30,50 +26,49 @@ class Logs {
    */
   static async get(uid: string, types: LogType[], range?: DateTimeRange): Promise<LogData | null> {
     const ref = FirestorePaths.Logs(uid);
-    try {
-      const doc = await ref.get();
-      if (!doc.exists) {
-        return null;
-      }
-      const data = doc.data();
-      if (!data) {
-        return null;
-      }
 
-      let result = {} as LogData;
-      // If range given, filter, else all
-      if (range) {
-        Object.keys(data).forEach((timestamp) => {
-          const messageObj = data[timestamp];
-          const messageDate = new Date(timestamp);
-          if (messageDate >= range.from && messageDate <= range.to) {
-            result[timestamp] = messageObj;
-          }
-        });
-      } else {
-        result = data;
-      }
-
-      // Bkp result
-      const bkpResult = result;
-      result = {} as LogData;
-      // If types give, filter, else all
-      if (types.length > 0) {
-        Object.keys(bkpResult).forEach((timestamp) => {
-          const messageObj = bkpResult[timestamp];
-          const messageType = messageObj.type;
-          if (types.includes(messageType as LogType)) {
-            result[timestamp] = messageObj;
-          }
-        });
-      } else {
-        result = bkpResult;
-      }
-
-      return result;
-    } catch (e) {
-      return Promise.reject(CustomApiError.create(500, e.message));
+    const doc = await ref.get();
+    if (!doc.exists) {
+      return null;
     }
+
+    const data = doc.data();
+    if (!data) {
+      return null;
+    }
+
+    let result = {} as LogData;
+    // If range given, filter, else all
+    if (range) {
+      Object.keys(data).forEach((timestamp) => {
+        const messageObj = data[timestamp];
+        const messageDate = new Date(timestamp);
+        if (messageDate >= range.from && messageDate <= range.to) {
+          result[timestamp] = messageObj;
+        }
+      });
+    } else {
+      result = data;
+    }
+
+    // Bkp result
+    const bkpResult = result;
+    result = {} as LogData;
+
+    // If types give, filter, else all
+    if (types.length > 0) {
+      Object.keys(bkpResult).forEach((timestamp) => {
+        const messageObj = bkpResult[timestamp];
+        const messageType = messageObj.type;
+        if (types.includes(messageType as LogType)) {
+          result[timestamp] = messageObj;
+        }
+      });
+    } else {
+      result = bkpResult;
+    }
+
+    return result;
   }
 }
 
