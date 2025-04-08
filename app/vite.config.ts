a66a27239ac293e5198ec05491525499878a257d
AvirukBasak <avirukbasak@gmail.com>
2025-04-08 14:52:37 +0530

    fix: do not load certificates when building

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,28 +4,38 @@ import eslint from "vite-plugin-eslint";
 import path from "path";
 import fs from "fs";
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ command }) => {
+  const config = {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync("../backend/certificates/localhost-key.pem"),
-      cert: fs.readFileSync("../backend/certificates/localhost.pem"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            return id.toString().split("node_modules/")[1].split("/")[0].toString();
-          }
+    server: void 0,
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes("node_modules")) {
+              return id.toString().split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
         },
       },
     },
-  },
-  plugins: [react(), eslint()],
+    plugins: [react(), eslint()],
+  };
+
+  // Only include HTTPS server settings during development, not during build
+  if (command === "serve") {
+    console.log("Load '../backend/certificates/*.pem'")
+    config.server = {
+      https: {
+        key: fs.readFileSync("../backend/certificates/localhost-key.pem"),
+        cert: fs.readFileSync("../backend/certificates/localhost.pem"),
+      },
+    };
+  }
+
+  return config;
 });
