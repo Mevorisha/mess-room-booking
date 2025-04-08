266300501f8fe07522c271e7d58d88658bcddaaa
AvirukBasak <avirukbasak@gmail.com>
2025-04-08 12:19:26 +0530

    feat: add config to allow all origin through cors

--- backend/src/lib/config.ts
+++ backend/src/lib/config.ts
@@ -4,6 +4,7 @@ if (!process.env.ENVIRONMENT_TYPE) throw new Error(".env ENVIRONMENT_TYPE undefi
 if (!process.env.API_SERVER_ORIGIN) throw new Error(".env API_SERVER_ORIGIN undefined");
 if (!process.env.WEB_SERVER_ORIGIN) throw new Error(".env WEB_SERVER_ORIGIN undefined");
 if (!process.env.CORS_ALLOWED_ORIGINS) throw new Error(".env CORS_ALLOWED_ORIGINS undefined");
+if (!process.env.CORS_ALLOW_EVERYTHING) throw new Error(".env CORS_ALLOW_EVERYTHING undefined");
 if (!process.env.FIREBASE_PROJECT_ID) throw new Error(".env FIREBASE_PROJECT_ID undefined");
 if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) throw new Error(".env FIREBASE_SERVICE_ACCOUNT_KEY undefined");
 if (!process.env.FIREBASE_DATABASE_URL) throw Error(".env FIREBASE_DATABASE_URL undefined");
@@ -23,6 +24,8 @@ export const WEB_SERVER_ORIGIN = process.env.WEB_SERVER_ORIGIN || "";
 export const CORS_ALLOWED_ORIGINS = JSON.parse(process.env.CORS_ALLOWED_ORIGINS || "[]") as string[];
 CORS_ALLOWED_ORIGINS.push(API_SERVER_ORIGIN);
 CORS_ALLOWED_ORIGINS.push(WEB_SERVER_ORIGIN);
+
+export const CORS_ALLOW_EVERYTHING = process.env.CORS_ALLOW_EVERYTHING === "true";
 
 export const IS_DEV = ["dev", "devnoemu"].includes(process.env.ENVIRONMENT_TYPE);
 export const RUN_ON_EMULATOR = /localhost|127\.0\.0\.1/i.test(API_SERVER_ORIGIN) && "devnoemu" !== process.env.ENVIRONMENT_TYPE;
