7b90eeff0308f1735831c6537f6a77731bf4ce59
AvirukBasak <avirukbasak@gmail.com>
2025-04-12 23:56:49 +0530

    fix: update logToDb function to log any error that falls outside client side errors

--- backend/src/middlewares/logToDb.ts
+++ backend/src/middlewares/logToDb.ts
@@ -2,11 +2,12 @@ import { CustomApiError } from "@/lib/utils/ApiError";
 import Logs from "@/models/Logs";
 
 export function logToDb(e: Error) {
-  if (e instanceof CustomApiError && e.status === 500) {
-    Logs.put("[InternalServerError]", {
-      timestamp: new Date().toUTCString(),
-      message: String(e),
-      type: "error",
-    }).catch((e) => console.error("[E] [LogToDb]", e));
+  if (e instanceof CustomApiError && 400 <= e.status && e.status <= 499) {
+    return;
   }
+  Logs.put("[InternalServerError]", {
+    timestamp: new Date().toUTCString(),
+    message: String(e),
+    type: "error",
+  }).catch((e) => console.error("[E] [LogToDb]", e));
 }
