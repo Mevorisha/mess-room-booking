aea2a6cb1011aaea57499f64f357534c4a2ab7fb
AvirukBasak <avirukbasak@gmail.com>
2025-04-12 11:57:42 +0530

    fix: replace uid based authenticate w/ getLoggedInUser coz no uid present in request data

--- backend/src/pages/api/rooms/[roomId]/restore/index.ts
+++ backend/src/pages/api/rooms/[roomId]/restore/index.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { respond } from "@/lib/utils/respond";
-import { authenticate } from "@/middlewares/auth";
+import { getLoggedInUser } from "@/middlewares/auth";
 import { withmiddleware } from "@/middlewares/withMiddleware";
 import { CustomApiError } from "@/lib/utils/ApiError";
 import { RateLimits } from "@/middlewares/rateLimit";
@@ -18,16 +18,15 @@ export default withmiddleware(async function PATCH(req: NextApiRequest, res: Nex
     throw CustomApiError.create(405, "Method Not Allowed");
   }
 
-  const uid = req.query["uid"] as string;
-  if (!uid) {
-    throw CustomApiError.create(400, "Missing field 'uid: string'");
-  }
   const roomId = req.query["roomId"] as string;
   if (!roomId) {
     throw CustomApiError.create(400, "Missing field 'roomId: string'");
   }
-  // Require authentication middleware
-  await authenticate(req, uid);
+
+  // Auth middleware to get user
+  const authResult = await getLoggedInUser(req);
+  // Automatically throws ApiError and is caught by catchAll (middleware)
+  const uid = authResult.getUid();
 
   if (!(await RateLimits.ROOM_RESTORE(uid, req, res))) return;
 
