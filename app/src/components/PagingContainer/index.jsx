d113289a14c3908b4ee28634de34866257b5aaee
AvirukBasak <avirukbasak@gmail.com>
2025-04-13 08:41:10 +0530

    feat: hide paging if no. of pages is 1

--- app/src/components/PagingContainer/index.jsx
+++ app/src/components/PagingContainer/index.jsx
@@ -4,89 +4,69 @@ import "./styles.css";
 
 /**
  * @param {{
-*   totalPages: number,
-*   currentPage: number,
-*   setCurrentPage:  React.Dispatch<React.SetStateAction<number>>,
-*   onPageChange: (n: number) => void
-* }} props
-*/
+ *   totalPages: number,
+ *   currentPage: number,
+ *   setCurrentPage:  React.Dispatch<React.SetStateAction<number>>,
+ *   onPageChange: (n: number) => void
+ * }} props
+ * @returns {React.JSX.Element}
+ */
 export default function PagingContainer({ totalPages, currentPage, setCurrentPage, onPageChange }) {
- const handlePageChange = (page) => {
-   if (page < 1 || page > totalPages) return;
-   setCurrentPage(page);
-   if (onPageChange) onPageChange(page);
- };
+  if (totalPages <= 1) return <></>;
 
- const renderPageNumbers = () => {
-   const pageNumbers = [];
-   let startPage, endPage;
+  function handlePageChange(/** @type {number} */ page) {
+    if (page < 1 || page > totalPages) return;
+    setCurrentPage(page);
+    if (onPageChange) onPageChange(page);
+  }
 
-   if (totalPages <= 5) {
-     // If total pages are 5 or less, show all pages
-     startPage = 1;
-     endPage = totalPages;
-   } else {
-     // Show 5 pages with current in the middle when possible
-     if (currentPage <= 3) {
-       startPage = 1;
-       endPage = 5;
-     } else if (currentPage >= totalPages - 2) {
-       startPage = totalPages - 4;
-       endPage = totalPages;
-     } else {
-       startPage = currentPage - 2;
-       endPage = currentPage + 2;
-     }
-   }
+  function renderPageNumbers() {
+    const pageNumbers = [];
+    let startPage, endPage;
+    if (totalPages <= 5) {
+      // If total pages are 5 or less, show all pages
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      // Show 5 pages with current in the middle when possible
+      if (currentPage <= 3) {
+        startPage = 1;
+        endPage = 5;
+      } else if (currentPage >= totalPages - 2) {
+        startPage = totalPages - 4;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 2;
+        endPage = currentPage + 2;
+      }
+    }
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(
+        <div className={`page-button ${i === currentPage ? "active" : ""}`} key={i} onClick={() => handlePageChange(i)}>
+          {i}
+        </div>
+      );
+    }
+    return pageNumbers;
+  }
 
-   for (let i = startPage; i <= endPage; i++) {
-     pageNumbers.push(
-       <div 
-         className={`page-button ${i === currentPage ? 'active' : ''}`}
-         key={i}
-         onClick={() => handlePageChange(i)}
-       >
-         {i}
-       </div>
-     );
-   }
+  return (
+    <div className="components-PagingContainer">
+      <div className="control-button first-page" onClick={() => handlePageChange(1)} title="First Page">
+        <i className="fa fa-angle-double-left"></i>
+      </div>
+      <div className="control-button prev-page" onClick={() => handlePageChange(currentPage - 1)} title="Previous Page">
+        <i className="fa fa-angle-left"></i>
+      </div>
 
-   return pageNumbers;
- };
+      {renderPageNumbers()}
 
- return (
-   <div className="components-PagingContainer">
-     <div 
-       className="control-button first-page" 
-       onClick={() => handlePageChange(1)}
-       title="First Page"
-     >
-       <i className="fa fa-angle-double-left"></i>
-     </div>
-     <div 
-       className="control-button prev-page" 
-       onClick={() => handlePageChange(currentPage - 1)}
-       title="Previous Page"
-     >
-       <i className="fa fa-angle-left"></i>
-     </div>
-     
-     {renderPageNumbers()}
-     
-     <div 
-       className="control-button next-page" 
-       onClick={() => handlePageChange(currentPage + 1)}
-       title="Next Page"
-     >
-       <i className="fa fa-angle-right"></i>
-     </div>
-     <div 
-       className="control-button last-page" 
-       onClick={() => handlePageChange(totalPages)}
-       title="Last Page"
-     >
-       <i className="fa fa-angle-double-right"></i>
-     </div>
-   </div>
- );
+      <div className="control-button next-page" onClick={() => handlePageChange(currentPage + 1)} title="Next Page">
+        <i className="fa fa-angle-right"></i>
+      </div>
+      <div className="control-button last-page" onClick={() => handlePageChange(totalPages)} title="Last Page">
+        <i className="fa fa-angle-double-right"></i>
+      </div>
+    </div>
+  );
 }
