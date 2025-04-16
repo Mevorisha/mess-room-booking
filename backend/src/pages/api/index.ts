0b3613fa0ace607d2c495b7c34fcbcef45553949
AvirukBasak <avirukbasak@gmail.com>
2025-04-16 19:32:58 +0530

    feat: implement initial Home page with layout and styles

--- backend/src/pages/api/index.ts
+++ backend/src/pages/api/index.ts
@@ -3,13 +3,13 @@ import { respond } from "@/lib/utils/respond";
 
 /**
  * ```
- * request = "GET /"
+ * request = "GET /api"
  *
  * response = {
  *   message: "Hello World!"
  * }
  * ```
  */
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
+export default async function GET(_req: NextApiRequest, res: NextApiResponse) {
   return respond(res, { status: 200, message: "Hello World!" });
 }
