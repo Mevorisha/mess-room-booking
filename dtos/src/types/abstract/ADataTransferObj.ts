2ad53283289a7fcc444692f4feb9ead715140f8e
AvirukBasak <avirukbasak@gmail.com>
2025-08-05 17:29:43 +0530

    refactor: enhance type constraints in ADataTransferObj methods for improved type safety

--- dtos/src/types/abstract/ADataTransferObj.ts
+++ dtos/src/types/abstract/ADataTransferObj.ts
@@ -2,10 +2,10 @@ import NetworkType from "@/NetworkType";
 import { ClassConstructor, instanceToPlain, plainToInstance } from "class-transformer";
 
 export default abstract class ADataTransferObj {
-  static toJson<T>(dto: T): NetworkType {
+  static toJson<T extends ADataTransferObj>(dto: T): NetworkType {
     return instanceToPlain(dto);
   }
-  static fromJson<T>(json: NetworkType, cls: ClassConstructor<T>): T {
+  static fromJson<T extends ADataTransferObj>(cls: ClassConstructor<T>, json: NetworkType): T {
     return plainToInstance(cls, json);
   }
 }
