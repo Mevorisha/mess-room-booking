b098085fd2d119969caf980ac70ec1e53cb3f676
AvirukBasak <avirukbasak@gmail.com>
2025-08-08 14:59:25 +0530

    updated /api/identityDocs/[uid]/GOV_ID/readImage to use sharedtypes DTO for query validation

--- backend/src/pages/api/identityDocs/[uid]/GOV_ID/readImage/index.ts
+++ backend/src/pages/api/identityDocs/[uid]/GOV_ID/readImage/index.ts
@@ -2,11 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Identity, { SchemaFields } from "@/models/Identity";
 import { getLoggedInUser } from "@/middlewares/Auth";
 import { WithMiddleware } from "@/middlewares/WithMiddleware";
-import { MultiSizeImageSz } from "@/firebase/init";
 import { gsPathToUrl } from "@/models/utils/gsUrlManager";
 import { CustomApiError } from "@/types/CustomApiError";
 import { RateLimits } from "@/middlewares/RateLimiter";
 import HeaderTypes from "@/types/HeaderTypes";
+import { IdentityReqReadImage } from "sharedtypes";
 
 /**
  * ```
@@ -21,25 +21,20 @@ export default WithMiddleware(async function GET(req: NextApiRequest, res: NextA
   if (req.method !== "GET") {
     throw CustomApiError.create(405, "Method Not Allowed");
   }
-  // Extract user ID from request
-  const uid = req.query["uid"] as string;
-  const size = req.query["size"] as MultiSizeImageSz;
-  const b64 = req.query["b64"] === "true" ? true : false;
-  if (!uid) {
-    throw CustomApiError.create(400, "Missing field 'uid: string'");
+
+  // Extract query params from request
+  const queryRes = IdentityReqReadImage.create(req.query);
+  if (queryRes.isErr) {
+    throw CustomApiError.create(400, queryRes.error.message);
   }
-  if (!size) {
-    throw CustomApiError.create(400, "Missing query 'size: small | medium | large'");
-  }
-  if (!["small", "medium", "large"].includes(size)) {
-    throw CustomApiError.create(400, "Invalid query 'size: small | medium | large'");
-  }
+  const { uid, size, b64 } = queryRes.value;
 
   const profile = await Identity.get(uid, "GS_PATH", [SchemaFields.IDENTITY_PHOTOS]);
-  if (!profile?.identityPhotos?.govId || !profile?.identityPhotos?.govId[size]) {
+  if (profile?.identityPhotos?.govId == null || profile.identityPhotos.govId[size] === "") {
     throw CustomApiError.create(404, "Image not found");
   }
-  if (profile.identityPhotos.workIdIsPrivate) {
+
+  if (profile.identityPhotos.workIdIsPrivate ?? false) {
     // Require authentication middleware
     const authResult = await getLoggedInUser(req);
     if (authResult.isSuccess() && authResult.getUid() !== uid) {
@@ -48,14 +43,17 @@ export default WithMiddleware(async function GET(req: NextApiRequest, res: NextA
     // Trigger ApiError
     if (!authResult.isSuccess()) authResult.getUid();
   }
+
   // Get image direct URL and send binary data
-  const directUrl = await gsPathToUrl(profile?.identityPhotos?.govId[size]);
+  const directUrl = await gsPathToUrl(profile.identityPhotos.govId[size]);
   const response = await fetch(directUrl);
   if (!response.ok) {
     throw CustomApiError.create(500, "Failed to fetch image");
   }
+
   const contentType = response.headers.get(HeaderTypes.CONTENT_TYPE);
   const imageBuffer = await response.arrayBuffer();
+
   if (b64) {
     res.setHeader(HeaderTypes.CONTENT_TYPE, "text/plain");
     res.setHeader(HeaderTypes.X_CONTENT_ENCODING, "BASE64");
