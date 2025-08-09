9e9ef4423975af88ba4da8f298d866c9a355dec4
AvirukBasak <avirukbasak@gmail.com>
2025-08-09 19:45:42 +0530

    MultiSizePhotoDTO: implmented static methods and made constructor private

--- sharedtypes/src/types/MultiSizePhotoDTO.ts
+++ sharedtypes/src/types/MultiSizePhotoDTO.ts
@@ -1,16 +1,20 @@
 import { IsString } from "class-validator";
 import { ADataTransferObj } from "./abstract/ADataTransferObj";
+import { DtoValidationError } from "./errors/DtoValidationError";
+import { Result } from "./Result";
+import { NetworkType } from "./NetworkType";
 
 export enum MultiSizePhotoErrors {
   INVALID_URL = "Image URL should be a string",
 }
 
-export interface MultiSizePhoto {
+interface ConstructorParams {
   small: string;
   medium: string;
   large: string;
 }
 
+export type MultiSizePhoto = ConstructorParams;
 export type MultiSizeImageSz = keyof MultiSizePhoto;
 
 export class MultiSizePhotoDTO extends ADataTransferObj implements MultiSizePhoto {
@@ -23,11 +27,23 @@ export class MultiSizePhotoDTO extends ADataTransferObj implements MultiSizePhot
   @IsString({ message: MultiSizePhotoErrors.INVALID_URL })
   large: string;
 
-  constructor(data: { small: string; medium: string; large: string }) {
+  private constructor(data: ConstructorParams) {
     super();
 
     this.small = data.small;
     this.medium = data.medium;
     this.large = data.large;
+  }
+
+  static override create(data: ConstructorParams): Result<MultiSizePhotoDTO, DtoValidationError> {
+    return ADataTransferObj._create(new this(data));
+  }
+
+  static override fromJson(json: NetworkType): Result<MultiSizePhotoDTO, DtoValidationError> {
+    return ADataTransferObj._fromJson(new this(json as MultiSizePhoto));
+  }
+
+  static override toJson(obj: MultiSizePhotoDTO): NetworkType {
+    return ADataTransferObj._toJson(obj);
   }
 }
