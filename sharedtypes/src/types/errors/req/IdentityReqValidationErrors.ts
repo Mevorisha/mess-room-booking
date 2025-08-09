a2c9ee1e5cca811465220ac9f7cd92ef65f77921
AvirukBasak <avirukbasak@gmail.com>
2025-08-09 19:44:18 +0530

    renamed error file: all identity errors in one file

--- sharedtypes/src/types/errors/req/IdentityReqValidationErrors.ts
+++ sharedtypes/src/types/errors/req/IdentityReqValidationErrors.ts
@@ -1,4 +1,4 @@
-export enum IdentityImageReqValidationErrors {
+export enum IdentityReqValidationErrors {
   MISSING_UID = "Missing UID",
   IMAGE_SIZE_INVALID = "Images size must be 'small', 'medium' or 'large'",
   INVALID_B64_FLAG = "Base 64 flag should be true or false",
