59c807ad4b13e9ce5a6c7b97700473844234ded9
AvirukBasak <avirukbasak@gmail.com>
2025-08-13 16:44:13 +0530

    updated identity read endpoint with new dtos

--- backend/src/pages/api/profile/[uid]/read/index.ts
+++ backend/src/pages/api/profile/[uid]/read/index.ts
@@ -1,10 +1,13 @@
+import z from "zod";
 import { NextApiRequest, NextApiResponse } from "next";
-import Identity, { PsudoFields, SchemaFields } from "@/models/Identity";
 import { respond } from "@/utils/respond";
 import { WithMiddleware } from "@/middlewares/WithMiddleware";
 import { getLoggedInUser } from "@/middlewares/Auth";
 import { CustomApiError } from "@/types/CustomApiError";
 import { RateLimits } from "@/middlewares/RateLimiter";
+import { RequestValidationParser } from "@/parsers/RequestValidationParser";
+import { IdentityRepo } from "@/repo/IdentityRepo";
+import { IdentityGetResBodyNoAuthDTO, IdentityGetResBodyWithAuthDTO } from "sharedtypes";
 
 /**
  * ```
@@ -48,46 +51,34 @@ import { RateLimits } from "@/middlewares/RateLimiter";
  * ```
  */
 export default WithMiddleware(async function GET(req: NextApiRequest, res: NextApiResponse) {
-  // Allow only GET requests
-  if (req.method !== "GET") {
-    throw CustomApiError.create(405, "Method Not Allowed");
-  }
-  // Extract UID from the query
-  const uid = req.query["uid"] as string;
-  if (!uid) {
-    throw CustomApiError.create(400, "Missing field 'uid: string'");
-  }
+  const { uid } = RequestValidationParser.parse({
+    req,
+    method: "GET",
+    params: z.object({
+      uid: RequestValidationParser.CommonSchema.UID,
+    }),
+  });
 
   if (!(await RateLimits.PROFILE_READ(req, res))) return;
-
-  const fields = [
-    PsudoFields.DISPLAY_NAME,
-    SchemaFields.FIRST_NAME,
-    SchemaFields.LAST_NAME,
-    SchemaFields.MOBILE,
-    SchemaFields.PROFILE_PHOTOS,
-  ];
 
   // If logged-in, send additional identity information
   const authResult = await getLoggedInUser(req);
   if (authResult.isSuccess()) {
     const loggedInUid = authResult.getUid();
     if (loggedInUid === uid) {
-      fields.push(
-        SchemaFields.EMAIL,
-        SchemaFields.TYPE,
-        SchemaFields.IDENTITY_PHOTOS,
-        SchemaFields.LANGUAGE,
-        SchemaFields.CREATED_ON,
-        SchemaFields.LAST_MODIFIED_ON,
-        SchemaFields.TTL
-      );
+      const result = await IdentityRepo.findById(uid, "API_URI", { auth: true });
+      if (result == null) {
+        throw CustomApiError.create(404, "User not found");
+      }
+      const json = IdentityGetResBodyWithAuthDTO.toJson(result);
+      return respond(res, { status: 200, json });
     }
   }
 
-  const result = await Identity.get(uid, "API_URI", fields);
-  if (!result) {
+  const result = await IdentityRepo.findById(uid, "API_URI", { auth: false });
+  if (result == null) {
     throw CustomApiError.create(404, "User not found");
   }
-  return respond(res, { status: 200, json: result });
+  const json = IdentityGetResBodyNoAuthDTO.toJson(result);
+  return respond(res, { status: 200, json });
 });
