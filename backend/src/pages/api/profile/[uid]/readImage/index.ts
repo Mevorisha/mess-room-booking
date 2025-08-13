91b6b8de2a0c05e945fde4f08b7b2ada092c0808
AvirukBasak <avirukbasak@gmail.com>
2025-08-13 20:35:11 +0530

    use new repo and dto api to update profile photo endpoint

--- backend/src/pages/api/profile/[uid]/readImage/index.ts
+++ backend/src/pages/api/profile/[uid]/readImage/index.ts
@@ -1,11 +1,12 @@
+import z from "zod";
 import { NextApiRequest, NextApiResponse } from "next";
-import Identity, { SchemaFields } from "@/models/Identity";
 import { WithMiddleware } from "@/middlewares/WithMiddleware";
-import { MultiSizeImageSz } from "@/firebase/init";
 import { gsPathToUrl } from "@/models/utils/gsUrlManager";
 import { CustomApiError } from "@/types/CustomApiError";
 import { RateLimits } from "@/middlewares/RateLimiter";
-import HeaderTypes  from "@/types/HeaderTypes";
+import HeaderTypes from "@/types/HeaderTypes";
+import { RequestValidationParser } from "@/parsers/RequestValidationParser";
+import { IdentityRepo } from "@/repo/IdentityRepo";
 
 /**
  * ```
@@ -14,38 +15,37 @@ import HeaderTypes  from "@/types/HeaderTypes";
  * ```
  */
 export default WithMiddleware(async function GET(req: NextApiRequest, res: NextApiResponse) {
-  // Only allow GET method
-  if (req.method !== "GET") {
-    throw CustomApiError.create(405, "Method Not Allowed");
-  }
-  // Extract user ID from request
-  const uid = req.query["uid"] as string;
-  const size = req.query["size"] as MultiSizeImageSz;
-  const b64 = req.query["b64"] === "true" ? true : false;
-  if (!uid) {
-    throw CustomApiError.create(400, "Missing field 'uid: string'");
-  }
-  if (!size) {
-    throw CustomApiError.create(400, "Missing query 'size: small | medium | large'");
-  }
-  if (!["small", "medium", "large"].includes(size)) {
-    throw CustomApiError.create(400, "Invalid query 'size: small | medium | large'");
-  }
+  // Extract query params from request
+  const { uid, size, b64 } = RequestValidationParser.parse({
+    req,
+    method: "GET",
+    params: z.object({
+      uid: RequestValidationParser.CommonSchema.UID,
+      size: RequestValidationParser.CommonSchema.IMAGE_SIZE,
+      b64: RequestValidationParser.CommonSchema.OPTIONAL_BOOL,
+    }),
+  });
 
   if (!(await RateLimits.PROFILE_PHOTO_READ(req, res))) return;
 
-  const profile = await Identity.get(uid, "GS_PATH", [SchemaFields.PROFILE_PHOTOS]);
-  if (!profile?.profilePhotos || !profile?.profilePhotos[size]) {
-    throw CustomApiError.create(404, "Image not found");
+  const profile = await IdentityRepo.findById(uid, "GS_PATH", { auth: true });
+  if (profile == null) {
+    throw CustomApiError.create(404, "User profile not found");
   }
+  if (profile.profilePhotos == null) {
+    throw CustomApiError.create(404, "Profile photo not found");
+  }
+
   // Get image direct URL and send binary data
-  const directUrl = await gsPathToUrl(profile?.profilePhotos[size]);
+  const directUrl = await gsPathToUrl(profile.profilePhotos[size]);
   const response = await fetch(directUrl);
   if (!response.ok) {
     throw CustomApiError.create(500, "Failed to fetch image");
   }
+
   const contentType = response.headers.get(HeaderTypes.CONTENT_TYPE);
   const imageBuffer = await response.arrayBuffer();
+
   if (b64) {
     res.setHeader(HeaderTypes.CONTENT_TYPE, "text/plain");
     res.setHeader(HeaderTypes.X_CONTENT_ENCODING, "BASE64");
