ca5731ebbc98cc3099aed52ca9964c9276690c7c
AvirukBasak <avirukbasak@gmail.com>
2025-08-15 17:22:24 +0530

    updated exports from sharedtypes

--- sharedtypes/src/index.ts
+++ sharedtypes/src/index.ts
@@ -1,10 +1,24 @@
-// Types
-export * from "./types/IdentityPhotosDTO.js";
-export * from "./types/MultiSizePhotoDTO.js";
-export * from "./types/typeEnums.js";
-export * from "./types/typeUnions.js";
-export * from "./types/NetworkType.js";
-export * from "./types/Result.js";
+// Booking DTOs
+export * from "./booking/BookingGetResBodyDTO.js";
+
+// Identity DTOs
+export * from "./identity/IdentityGetResBodyNoAuthDTO.js";
+export * from "./identity/IdentityGetResBodyWithAuthDTO.js";
+export * from "./identity/IdentityPatchImageVisibilityDTO.js";
+export * from "./identity/IdentityPostReqBodyDTO.js";
+
+// Logs DTOs
+export * from "./logs/LogPostReqBodyDTO.js";
+
+// Profile DTOs
+export * from "./profile/ProfilePatchReqBodyDTO.js";
+
+// Room DTOs
+export * from "./room/RoomGetResBodyNotOwnerDTO.js";
+export * from "./room/RoomGetResBodyOwnerDTO.js";
+export * from "./room/RoomPatchReqBodyDTO.js";
+export * from "./room/RoomPhotoUploadDTO.js";
+export * from "./room/RoomPostReqBodyDTO.js";
 
 // Abstract classes
 export * from "./types/abstract/ADataTransferObj.js";
@@ -12,44 +26,11 @@ export * from "./types/abstract/ADataTransferObj.js";
 // Error types
 export * from "./types/errors/DtoValidationError.js";
 
-// Booking DTOs
-export * from "./booking/BookingGetResBodyDTO.js";
-
-// Room DTOs
-export * from "./room/RoomGetResBodyOwnerDTO.js";
-export * from "./room/RoomGetResBodyNotOwnerDTO.js";
-
-// Identity DTOs
-export * from "./identity/IdentityGetResBodyWithAuthDTO.js";
-export * from "./identity/IdentityGetResBodyNoAuthDTO.js";
-export * from "./identity/IdentityPatchImageVisibilityDTO.js";
-export * from "./identity/IdentityPostReqBodyDTO.js";
-
-// Profile DTOs
-export * from "./profile/ProfilePatchReqBodyDTO.js";
-
-// Logs DTOs
-export * from "./logs/LogPostReqBodyDTO.js";
-
-// Re-export commonly used validator decorators for convenience
-export {
-  IsString,
-  IsNumber,
-  IsBoolean,
-  IsOptional,
-  IsArray,
-  IsObject,
-  ValidateNested,
-  IsNotEmpty,
-  IsEmail,
-  IsUrl,
-  IsUUID,
-  ArrayMinSize,
-  ArrayMaxSize,
-  MinLength,
-  MaxLength,
-  Min,
-  Max,
-} from "class-validator";
-
-export { Type, Transform, Exclude, Expose, plainToClass, classToPlain } from "class-transformer";
+// Types
+export * from "./types/IdentityPhotosDTO.js";
+export * from "./types/MultiSizePhotoDTO.js";
+export * from "./types/NetworkType.js";
+export * from "./types/NextJsTypes.js";
+export * from "./types/Result.js";
+export * from "./types/typeEnums.js";
+export * from "./types/typeUnions.js";
