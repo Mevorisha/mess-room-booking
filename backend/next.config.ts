ea91d822f351cd4f774eb0b3b06826edbdefa437
AvirukBasak <avirukbasak@gmail.com>
2025-08-20 07:46:49 +0530

    update nextjs config: since turbopack is used in dev, add config to resolve shared packages

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -1,8 +1,29 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  logging: false
+  logging: false,
+
+  experimental: {
+    externalDir: true,
+    turbo: {
+      root: path.join(__dirname, ".."),
+      resolveAlias: {
+        sharedtypes: "sharedtypes/dist",
+      },
+    },
+  },
+
+  webpack: (config, { isServer }) => {
+    // Add alias for sharedtypes
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharedtypes: path.resolve(__dirname, "../sharedtypes/dist"),
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
