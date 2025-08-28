7dd24921ffb950801ca5db094783b0bf4001a140
AvirukBasak <avirukbasak@gmail.com>
2025-08-29 05:08:53 +0530

    refactored index error handler

--- backend/src/middlewares/ErrorHandler/index.ts
+++ backend/src/middlewares/ErrorHandler/index.ts
@@ -2,29 +2,29 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { respond } from "@/utils/respond";
 import { CustomApiError } from "@/types/CustomApiError";
 import { logToDb } from "../LogToDb";
-import { handleFirebaseIndexError } from "./mkIndex";
+import { handleFbIdxErrOrBuildIdx, isFirebaseIndexError } from "./mkIndex";
 import { consoleLog } from "../ConsoleLog";
+import { Nullable } from "sharedtypes";
 
-export type FirebaseIndexErrorType = Error & { code: number; details: string };
-
-async function handleErr(e: FirebaseIndexErrorType | null, res: NextApiResponse) {
+async function handleErr(e: Nullable<Error>, res: NextApiResponse) {
   if (e == null) {
+    console.error("[E] [CatchAll] Error object is null");
     return respond(res, { status: 500, error: "Internal Server Error" });
   }
   if (e instanceof CustomApiError) {
     respond(res, { status: e.status, error: e.message });
     console.error(e);
-  } else {
+  } else if (isFirebaseIndexError(e)) {
     try {
-      const isErrorHandled = await handleFirebaseIndexError(e);
-      if (!isErrorHandled) {
-        // Not an index related error
-        respond(res, { status: 500, error: "Internal Server Error" });
-        console.error(e);
-      } else {
+      const status = await handleFbIdxErrOrBuildIdx(e);
+      if (status === "BUILD_IN_PROGRESS" || status === "BUILD_INITIATED") {
         // Index related error, ask user to wait
         respond(res, { status: 500, error: "Server busy. Please try again later." });
         console.log("[I] [CatchAll] Waiting for index to be created");
+      } else {
+        // Index related error, ask user to wait
+        respond(res, { status: 500, error: "Internal Server Error" });
+        console.log("[I] [CatchAll] Invalid (non-index) error passed to [HandleFbIdxErrOrBuildIdx]");
       }
     } catch (e) {
       // could call handleErr recursively here but that's a bad idea
@@ -39,6 +39,10 @@ async function handleErr(e: FirebaseIndexErrorType | null, res: NextApiResponse)
         console.error(e);
       }
     }
+  } else {
+    // Either not index related error or is not firebase error
+    respond(res, { status: 500, error: "Internal Server Error" });
+    console.error(e);
   }
 }
 
@@ -54,13 +58,13 @@ export function catchAll(
         prom
           .then(() => consoleLog(req, res))
           .then(() => resolve())
-          .catch((e: FirebaseIndexErrorType) =>
+          .catch((e: Error) =>
             logToDb(e)
               .then(() => handleErr(e, res))
               .then(() => consoleLog(req, res))
           );
     } catch (e) {
-      const error = e as FirebaseIndexErrorType;
+      const error = e instanceof Error ? e : CustomApiError.create(500, "Internal Server Error", e);
       void logToDb(error)
         .then(() => handleErr(error, res))
         .then(() => consoleLog(req, res));
