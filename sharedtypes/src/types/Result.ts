3063dbc9b0e3e42849c21015d017fba7ed37cffc
AvirukBasak <avirukbasak@gmail.com>
2025-08-30 01:39:54 +0530

    added an unwrap method in result; removed unused methods

--- sharedtypes/src/types/Result.ts
+++ sharedtypes/src/types/Result.ts
@@ -1,3 +1,5 @@
+import { Unsafe } from "./utils";
+
 /**
  * A result type that represents either a success (`Ok`) or a failure (`Err`).
  * Similar to Rust's Result or functional languages' Either.
@@ -28,8 +30,7 @@ export class Result<T, E extends Error> {
    *
    * @example
    * ```ts
-   * const error = new Error("Something went wrong");
-   * const result = Result.err(error);
+   * const result = Result.err(new Error("Something went wrong"));
    * if (result.isErr) {
    *   console.error(result.error.message); // "Something went wrong"
    * }
@@ -88,33 +89,6 @@ export class Result<T, E extends Error> {
   }
 
   /**
-   * Maps the success value to a new result.
-   *
-   * @example
-   * ```ts
-   * const result = Result.ok(2).map(x => x * 3); // Ok(6)
-   * ```
-   */
-  map<U>(fn: (value: T) => U): Result<U, E> {
-    return this.isOk ? Result.ok<U, E>(fn(this.value)) : Result.err<U, E>(this.error);
-  }
-
-  /**
-   * Maps the error to a new error type.
-   *
-   * @example
-   * ```ts
-   * class CustomError extends Error {}
-   * const result = Result.err(new Error("fail")).mapErr(
-   *   err => new CustomError(err.message)
-   * );
-   * ```
-   */
-  mapErr<F extends Error>(fn: (error: E) => F): Result<T, F> {
-    return this.isErr ? Result.err<T, F>(fn(this.error)) : Result.ok<T, F>(this.value);
-  }
-
-  /**
    * Returns the value if `Ok`, otherwise throws the contained error
    * @throws {E} The contained error
    *
@@ -140,6 +114,38 @@ export class Result<T, E extends Error> {
    */
   unwrapOr(defaultValue: T): T {
     return this.isOk ? this.value : defaultValue;
+  }
+
+  /**
+   * Returns union of value if `Ok` or error otherwise. Very useful if you don't
+   * want to extract inner members out of the Result due to aesthetic reasons.
+   *
+   * NOTE: Error type should be a `class` and NOT `interface` or `type`.
+   *
+   * ⚠️ UNSAFE: This requires using `instanceof` which IMO is lacking in type safety.
+   * For example, if you get a `Unsafe<T|E>`, you can do `instanceof U` without issues
+   * even though the returned value is either `T` or `E` and not `U`. Of course,
+   * TS doesn't let you access the `T` unless `E` is handled.
+   *
+   * @example
+   * ```ts
+   * fnThatReturnsResult(): Result<MyValue, MyError>;
+   *
+   * const { UNSAFE: result }: Unsafe<MyValue | MyError> = fnThatReturnsResult().unwrap();
+   *
+   * if (result instanceof MyError) {
+   *   // result is definitely of MyError type
+   * } else  {
+   *   // result is definitely of MyValue type
+   * }
+   * ```
+   */
+  unwrap(): Unsafe<T | E> {
+    if (this.isOk) {
+      return { UNSAFE: this.value };
+    } else {
+      return { UNSAFE: this.error };
+    }
   }
 
   /**
