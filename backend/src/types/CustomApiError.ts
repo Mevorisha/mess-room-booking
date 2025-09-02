da4d4770508351aae38e44127087c0d069f85cbd
AvirukBasak <avirukbasak@gmail.com>
2025-09-03 00:36:54 +0530

    updated custom api error: possible issue due to newly introduce options param in ErrorConstructor in lib: ESNEXT

--- backend/src/types/CustomApiError.ts
+++ backend/src/types/CustomApiError.ts
@@ -2,17 +2,21 @@ import { MultipleErrors } from "sharedtypes";
 
 export class CustomApiError extends Error {
   status: number;
+  override cause: unknown;
 
   private constructor(status: number, message: string, cause?: unknown) {
-    if (Array.isArray(cause)) {
-      super(message, { cause: new MultipleErrors(cause) });
-    } else if (cause instanceof Error) {
-      super(message, { cause });
-    } else {
-      super(message, { cause: JSON.stringify(cause, null, 2) });
-    }
+    super(message);
+
     this.name = `CustomApiError [${status}]`;
     this.status = status;
+
+    if (Array.isArray(cause)) {
+      this.cause = new MultipleErrors(cause);
+    } else if (cause instanceof Error || typeof cause === "string") {
+      this.cause = cause;
+    } else {
+      this.cause = JSON.stringify(cause, null, 2);
+    }
   }
 
   static create(status: number, message: string, cause?: unknown): CustomApiError {
