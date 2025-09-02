ca65e66c45ef935bc9c357f068cc3f97dc2892f1
AvirukBasak <avirukbasak@gmail.com>
2025-09-02 17:45:16 +0530

    bugfix: run job scheduler with handler; not on nextjs startup

--- backend/src/middlewares/WithMiddleware.ts
+++ backend/src/middlewares/WithMiddleware.ts
@@ -9,23 +9,22 @@ export function WithMiddleware(
   handler: (req: NextApiRequest, res: NextApiResponse) => Promise<NextApiResponse | undefined | void>
 ): (req: NextApiRequest, res: NextApiResponse) => Promise<void> {
   // wrap API handler in catchAll
-  const res = async (req: NextApiRequest, res: NextApiResponse) =>
+  return async (req: NextApiRequest, res: NextApiResponse) =>
     catchAll(req, res, async (req: NextApiRequest, res: NextApiResponse) => {
       if (!(await cors(req, res))) return;
+
       // if (!(await rateLimiter(50, null,  null, req, res))) return;
+
+      // Schedule all jobs
+      scheduleJobs();
+
+      // Run scheduled jobs on each API call
+      // This ensures jobs get checked regularly without needing a separate process
+      JobScheduler.getInstance()
+        .run()
+        .then(() => console.log("[I] [WithMiddleware] Invoked JobScheduler"))
+        .catch((err) => console.error("[E] [WithMiddleware] JobScheduler:", err));
+
       return handler(req, res);
     });
-
-  // Schedule all jobs
-  scheduleJobs();
-
-  // Run scheduled jobs on each API call
-  // This ensures jobs get checked regularly without needing a separate process
-  JobScheduler.getInstance()
-    .run()
-    .then(() => console.log("[I] [WithMiddleware] Invoked JobScheduler"))
-    .catch((err) => console.error("[E] [WithMiddleware] JobScheduler:", err));
-
-  // return API handler wrapped in catchAll
-  return res;
 }
