174042f488cf6d3ce1e38f84355280a388c71887
AvirukBasak <avirukbasak@gmail.com>
2025-09-02 09:40:47 +0530

    fixed ts error: missing switch-case branches

--- app/src/modules/classes/User.ts
+++ app/src/modules/classes/User.ts
@@ -70,6 +70,8 @@ export default class IdentityWrapper {
         return this.identity?.profilePhotos as never;
       case "identityPhotos":
         return this.identity?.identityPhotos as never;
+      default:
+        return null;
     }
   }
 
